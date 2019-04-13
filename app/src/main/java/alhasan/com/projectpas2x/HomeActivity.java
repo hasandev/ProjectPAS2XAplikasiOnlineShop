c6fa81d652b1249b3b121a95af28d44f1db427b0
Al-Hasan Musthafa <alhasanmusthafa17.gmail.com>
2019-04-13 21:28:43 +0700

    Update 1

---
+++ app/src/main/java/alhasan/com/projectpas2x/HomeActivity.java
@@ -0,0 +1,13 @@
+package alhasan.com.projectpas2x;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/alhasan/com/projectpas2x/MainActivity.java
@@ -0,0 +1,32 @@
+package alhasan.com.projectpas2x;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+    //4000=4 detik
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home=new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+    }
+}
+
